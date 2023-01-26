9767141a7888ceab8e705c94b21649e7d1cfbeb5
Matheus Macedo <matheusg3.201168@gmail.com>
2023-01-25 21:34:17 -0300

    background refactoring

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,11 @@
 const button = document.querySelector('.botao-trailer')
 const modal =  document.querySelector('.modal')
+const fecharModal = document.querySelector('.fechar-modal')
 
 
 button.addEventListener('click', () =>{
    modal.classList.add('aberto')
 })
-
-const fecharModal = document.querySelector('.fechar-modal')
 
 fecharModal.addEventListener('click', () =>{
     const modalComAberto = document.querySelector('.modal.aberto')
